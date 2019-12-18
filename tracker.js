5a4e48fdeb853f41d9d78ecda88075362f6b85ac
Electronixxx <51324472+electronixxx@users.noreply.github.com>
2019-12-18 18:00:49 -0500

    Add files via upload
    JavaScript code to be executed in Console.

---
+++ tracker.js
@@ -0,0 +1,38 @@
+var checked = 0;
+var last_time;
+
+function get_time_diff () {
+    var currDate = new Date();
+    var dif = currDate.getTime() - last_time.getTime();
+    var TimeInSeconds = Math.abs(dif/1000);
+
+    var MINUTES = Math.floor(TimeInSeconds/60)
+    var SECONDS = Math.floor(TimeInSeconds%60);
+    var FinalTime = 'Online for: ';
+
+    if (MINUTES != 0)
+        FinalTime = FinalTime + MINUTES + ' Minutes, ';
+    FinalTime += SECONDS + ' Seconds.'
+    console.log(FinalTime);
+}
+
+setInterval(function() {
+    var date = new Date();
+	var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	try {
+        var last_seen = document.getElementsByClassName("i1XSV")[0].innerText;
+        if (checked === 0) {
+            last_time = new Date(date);
+            console.log(time + ' ' + last_seen);
+            checked = 1;
+        }
+	}
+	catch(err) {
+		if (checked === 1) {
+            checked = 0;
+            console.log(time + ' offline');
+            get_time_diff();
+            console.log('------------------');
+        }
+	}
+}, 1000);
